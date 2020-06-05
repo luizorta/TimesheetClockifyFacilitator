8883f141d69923fcded8953d21268944c20ed3d7
Luiz <luizorta@gmail.com>
2020-06-05 16:30:28 -0300

    atualizacao

--- src/br/com/mjv/dto/Atividade.java
+++ src/br/com/mjv/dto/Atividade.java
@@ -7,6 +7,12 @@ public class Atividade {
 	private String descricao;
 	private String nomeProjeto;
 	
+	private String horario1Entrada;
+	private String horario1Saida;
+	
+	private String horario2Entrada;
+	private String horario2Saida;
+	
 	public Atividade() {
 		
 	}
@@ -16,6 +22,40 @@ public class Atividade {
 		this.totalHoras = totalHoras;
 		this.descricao = descricao;
 		this.nomeProjeto = nomeProjeto;
+	}
+	
+	
+
+	public String getHorario1Entrada() {
+		return horario1Entrada;
+	}
+
+	public String getHorario1Saida() {
+		return horario1Saida;
+	}
+
+	public String getHorario2Entrada() {
+		return horario2Entrada;
+	}
+
+	public String getHorario2Saida() {
+		return horario2Saida;
+	}
+
+	public void setHorario1Entrada(String horario1Entrada) {
+		this.horario1Entrada = horario1Entrada;
+	}
+
+	public void setHorario1Saida(String horario1Saida) {
+		this.horario1Saida = horario1Saida;
+	}
+
+	public void setHorario2Entrada(String horario2Entrada) {
+		this.horario2Entrada = horario2Entrada;
+	}
+
+	public void setHorario2Saida(String horario2Saida) {
+		this.horario2Saida = horario2Saida;
 	}
 
 	public String getDescricao() {
